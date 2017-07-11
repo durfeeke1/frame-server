e0f3b26ce3b2ec884625fa6e147a7f54495b73b1
Kevin Durfee <kevdurfee@gmail.com>
2017-07-10 23:02:25 -0700

    execute system command after post

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
 
+// in ms
+const DELAY_BEFORE_REBOOT = 500;
+
 const ERR_CODE_UPROCESSABLE_ENTITY = 422;
 const SUCCESS_CODE = 200;
+
+function rebootBoard(){
+  var exec = require('child_process').exec;
+  var cmd = 'echo MAKING SYSTEM CALL';
+
+  exec(cmd, function(error, stdout, stderr) {
+    console.log(stdout);
+  });
+}
 
 module.exports = function(app) {
   /* Render the landing page */
@@ -25,6 +37,7 @@ module.exports = function(app) {
       console.log("The file was saved!");
       response.send(SUCCESS_CODE);
 
+      setTimeout(rebootBoard, DELAY_BEFORE_REBOOT);
     });
 
     console.log(request.body)
